3af48ed30fb8d8428c81accdd5c1a17e5e8a3fcf
rahul-8901 <52934279+rahul-8901@users.noreply.github.com>
2021-02-03 17:27:23 +0530

    Number completed

--- 01Basics/01Numbers.py
+++ 01Basics/01Numbers.py
@@ -2,3 +2,6 @@ print(2+2)
 print(50 - 5*6)
 print((50 - 5*6) / 4)
 print(8 / 5) #division always returns a floating point number
+print(8//5) #to do a floor division
+print(17%3) #returns the remainder
+print(5 ** 2) #returns 5 power of 2
